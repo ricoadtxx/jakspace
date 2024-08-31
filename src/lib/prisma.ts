d484a8107008880e92787b9c441ffc566f2862a3
ricoadtxx <euricoadit555@gmail.com>
2024-09-01 00:05:17 +0700

    UI: Add Fitur Dashboard and 3D Background

--- src/lib/prisma.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-
-// Inisialisasi Prisma Client
-const prisma = new PrismaClient();
-
-// Ekspor Prisma Client untuk digunakan di tempat lain
-export default prisma;
